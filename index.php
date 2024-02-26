ea83fa533f4a56d0f8b88f5c0a75ba8a7fb69dec
LukePadawalker <padurariu.luca@gmail.com>
2024-02-26 17:22:24 +0200

    added all products

--- index.php
+++ index.php
@@ -35,7 +35,25 @@ class animal extends type
 }
 
 
-$Royal_Canin_Mini_Adult = new product(30, 'Food', 'Animal');
+$Royal_Canin_Mini_Adult = new product(30, 'Food', 'Dog');
+$Almo_Nature_Holistic = new product(20, 'Food', 'Dog');
+$Almo_Nature_Cat = new product(45, 'Food', 'Fish');
+$Mangime_per_Pesci_Guppy = new product(15, 'Sit', 'Bird');
+$Voliera_Wilma = new product(19, 'Filter', 'Fish');
+$Cartucce_Filtranti = new product(29, 'Toy', 'Dog');
+$Kong_Classic = new product(23, 'Toy', 'Cat');
+$Topini_di_peluche = new product(23, 'Toy', 'Cat');
+
+
+
+
+
+
+
+
+
+
+var_dump($Royal_Canin_Mini_Adult);
 
 
 ?>
